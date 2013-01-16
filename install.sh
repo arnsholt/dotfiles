4868eb44d03f2ca5ee15fd0800159451ce8dc551
Arne Skjærholt <arnsholt@gmail.com>
2013-01-16 11:49:44 +0100

    Update install script to skip files already linked.

--- install.sh
+++ install.sh
@@ -7,13 +7,23 @@ now=$(date '+%Y%m%d%H%M')
 for file in dot.*; do
     file=$(echo "$file" | sed 's/^dot//')
     fullfile="$HOME/$file"
+    dotfile="$PWD/dot$file"
+
+    # Ignore files that are already linked to the repository.
+    if [ -L "$fullfile" ]; then
+        realfile=$(readlink -f "$fullfile")
+        if [ "$realfile" = "$dotfile" ]; then
+            echo "# Ignoring $file: Already linked";
+            continue
+        fi
+    fi
 
     if [ -e "$fullfile" ]; then
         echo "# Making backup of $file"
         mv "$fullfile" "$fullfile.$now"
     fi
 
-    ln -s "$PWD/dot$file" "$fullfile"
+    ln -s "$dotfile" "$fullfile"
 
     # The .ssh directory and its contents require some special logic, since
     # git doesn't respect modes (AFAICT).
