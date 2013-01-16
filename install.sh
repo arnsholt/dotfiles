614f58d48575cf7f2ad1ef3796f5498d07169872
Arne Skjærholt <arnsholt@gmail.com>
2013-01-16 08:59:27 +0100

    Actually install stuff in install.sh, fix permissions in .ssh/*.

--- install.sh
+++ install.sh
@@ -7,7 +7,19 @@ now=$(date '+%Y%m%d%H%M')
 for file in dot.*; do
     file=$(echo "$file" | sed 's/^dot//')
     fullfile="$HOME/$file"
-    echo "# Making backup of $file"
-    echo mv "$fullfile" "$fullfile.$now"
-    echo ln -s "$PWD/dot$file" "$fullfile"
+
+    if [ -e "$fullfile" ]; then
+        echo "# Making backup of $file"
+        mv "$fullfile" "$fullfile.$now"
+    fi
+
+    ln -s "$PWD/dot$file" "$fullfile"
+
+    # The .ssh directory and its contents require some special logic, since
+    # git doesn't respect modes (AFAICT).
+    if [ "$file" = ".ssh" ]; then
+        echo "Fixing modes on ~/.ssh/*"
+        chmod 700 $HOME/.ssh
+        chmod 600 $HOME/.ssh/*
+    fi
 done
