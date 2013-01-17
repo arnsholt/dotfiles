c3c44cb34ca7e0ac9484adb91783dcaf9e988ce3
Arne Skjærholt <arnsholt@gmail.com>
2013-01-17 11:09:14 +0100

    Refactor install script.

--- install.sh
+++ install.sh
@@ -1,33 +1,46 @@
 #!/bin/bash
 
-WHERE=$(dirname $0)
-cd "$WHERE"
+function backup() {
+    file=$1
+    shortname=$(basename "$file")
+    echo "Making backup of $shortname"
+    mv "$file" "$file.$now"
+}
 
-now=$(date '+%Y%m%d%H%M')
-for file in dot.*; do
-    file=$(echo "$file" | sed 's/^dot//')
-    fullfile="$HOME/$file"
-    dotfile="$PWD/dot$file"
+# Install $1 to directory $2 with name $3
+function install() {
+    src=$1
+    target=$3
+    fullfile="$2/$target"
 
     # Ignore files that are already linked to the repository.
     if [ -L "$fullfile" ]; then
         realfile=$(readlink -f "$fullfile")
-        if [ "$realfile" = "$dotfile" ]; then
-            echo "# Ignoring $file: Already linked";
+        if [ "$realfile" = "$PWD/$src" ]; then
+            echo "# Ignoring $target: Already linked";
             continue
         fi
     fi
 
     if [ -e "$fullfile" ]; then
-        echo "# Making backup of $file"
-        mv "$fullfile" "$fullfile.$now"
+        backup "$fullfile"
     fi
 
-    ln -s "$dotfile" "$fullfile"
+    ln -s "$PWD/$src" "$fullfile"
+}
+
+WHERE=$(dirname $0)
+cd "$WHERE"
+
+now=$(date '+%Y%m%d%H%M')
+for file in dot.*; do
+    shortname=$(echo "$file" | sed 's/^dot//')
+
+    install "$file" "$HOME" "$shortname"
 
     # The .ssh directory and its contents require some special logic, since
     # git doesn't respect modes (AFAICT).
-    if [ "$file" = ".ssh" ]; then
+    if [ "$shortname" = ".ssh" ]; then
         echo "Fixing modes on ~/.ssh/*"
         chmod 700 $HOME/.ssh
         chmod 600 $HOME/.ssh/*
